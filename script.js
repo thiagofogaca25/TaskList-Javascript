b3a857cba7cdb2ad676aa1882e541386c8ead382
Thiago Fogaca <thiagofogaca25@gmail.com>
2023-06-09 21:30:45 -0300

    add function to edit button

--- script.js
+++ script.js
@@ -23,7 +23,11 @@ function addTask(taskText) {
   var editButton = document.createElement("button");
   editButton.className = "btn btn-info rounded-5 text-white w-25 p-0 m-1";
   editButton.innerText = 'edit';
-  
+  editButton.addEventListener("click",function(){
+    editHandleFormSubmit(taskText);
+    
+  })
+
   var deleteButton = document.createElement("button");
   deleteButton.textContent = "X";
   deleteButton.className = "btn btn-danger rounded-5 w-25 p-0 m-1";
@@ -32,13 +36,28 @@ function addTask(taskText) {
     deleteTask(taskText);
   });
 
-  var hrbuttons = document.createElement('hr');
+
+  var hrList = document.createElement('hr');
 
   divButtons.appendChild(deleteButton);
   divButtons.appendChild(editButton);
   li.appendChild(divButtons);
+  li.appendChild(hrList);
   taskList.appendChild(li);
-  taskList.appendChild(hrbuttons);
+  
+}
+
+function editTask(index,name){
+  var tasks = loadTasks();
+  
+
+  if (index !== -1) {
+    tasks[index]= name;
+    newTasks = [...tasks]
+    saveTasks(newTasks)
+    console.log(newTasks);
+  }
+  location.reload();
 }
 
 function deleteTask(taskText) {
@@ -65,6 +84,80 @@ function handleFormSubmit(event) {
     taskInput.value = "";
   }
 }
+
+function editHandleFormSubmit(taskText) {
+  
+  var items = document.querySelectorAll('.item');
+  items.forEach(function(item) {
+    item.style.display = 'none';
+  });
+
+  var tasks = loadTasks();
+  var index = tasks.indexOf(taskText);
+
+  var divformedit = document.createElement('div');
+  divformedit.className = "position-fixed center-form ";
+
+  var form = document.createElement('form');
+
+  var formButtons = document.createElement('div');
+  formButtons.className = 'd-flex justify-content-between'
+
+  var nameLabel = document.createElement('label');
+  nameLabel.className = 'w-100 text-center'
+  nameLabel.textContent = 'Editar';
+
+  var nameInput = document.createElement('input');
+  nameInput.className = 'input-tasktitle mb-2 mt-2 p-3'
+  nameInput.setAttribute('type', 'text');
+  nameInput.setAttribute('name', 'name');
+  nameInput.placeholder = taskText;
+
+  divformedit.appendChild(form);
+  form.appendChild(nameLabel);
+  form.appendChild(nameInput);
+  form.appendChild(formButtons);
+
+  var cancelButton = document.createElement('button');
+  cancelButton.className = 'btn btn-danger'
+  cancelButton.textContent = 'Cancelar';
+  cancelButton.addEventListener('click', function(event) {
+  
+    event.preventDefault();
+    items.forEach(function(item) {
+      item.style.display = 'block';
+    });
+   
+    form.remove();
+  });
+  formButtons.appendChild(cancelButton);
+
+  var saveButton = document.createElement('button');
+  saveButton.textContent = 'Salvar';
+  saveButton.className = 'btn btn-success'
+  saveButton.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    var name = nameInput.value;
+ 
+    if (name !== "") {
+      editTask(index,name);
+      console.log(name);
+    }
+
+    items.forEach(function(item) {
+      item.style.display = 'block';
+    });
+
+    form.remove();
+  });
+  formButtons.appendChild(saveButton);
+
+
+  var container = document.querySelector('.container');
+  container.appendChild(divformedit);
+}
+
 
 window.addEventListener("load", function() {
   var tasks = loadTasks();
